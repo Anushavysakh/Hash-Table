742a526911e031127c280c79780938b9efc8b12e
Anusha <anushavysakh232@gmail.com>
2022-08-15 23:26:20 +0530

    Uc2 committed

--- HashTableMain.java
+++ HashTableMain.java
@@ -1,27 +1,28 @@
 package com.hashtable.bridgelabz;
 
 public class HashTableMain {
+    public static void main(String[] args) {
+        HashtableImpl<String, Integer> hashImpl = new HashtableImpl();
+        String message = "Paranoids are not paranoid because they are paranoid but " +
+                "because they keep putting themselves deliberately into paranoid avoidable situations";
 
-	    public static void main(String[] args) {
-	        HashtableImpl<String, Integer> hashImpl = new HashtableImpl();
-	        String message = "To be or not to be";
+      
+        String[] messageArray = message.toLowerCase().split(" ");
 
-	        //Split the words when a white space comes up.
-	        //Convert all the words in lowercase
-	        String[] messageArray = message.toLowerCase().split(" ");
+        for(String word: messageArray) {
+            //Search for the word in the linked list
+            Integer value =  hashImpl.get(word);
+            //If not found, frequency of the word StayWith 1
+            if( value == null)
+                value = 1;
+            else
+                //If found, frequency of the word increases by 1
+                value = value + 1;
+            hashImpl.add(word , value);
+        }
+        System.out.println(hashImpl);
+    }
 
-	        for(String word: messageArray) {
-	            //Search for the word in the linked list
-	            Integer value =  hashImpl.get(word);
-	            //If not found, frequency of the word StayWith 1
-	            if( value == null)
-	                value = 1;
-	            else
-	                //If found, frequency of the word increases by 1
-	                value = value + 1;
-	            hashImpl.add(word , value);
-	        }
-	        System.out.println(hashImpl);
-	    }
 }
+
 
